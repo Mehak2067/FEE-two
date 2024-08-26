405ee77b62153def06aad46305f9c29449d8273e
Mehak2067 <mehak2067.be23@chitkara.edu.in>
2024-08-26 13:16:56 +0530

    updated

---
+++ Lectures/JS/datatypes.js
@@ -0,0 +1,16 @@
+let a=10;
+console.log(typeof (a));
+let fruit="apple";
+console.log(typeof (fruit));
+
+let x=true;
+console.log(typeof (x));
+let y;
+console.log(typeof (y));
+y=20;
+console.log(typeof (y));
+let j =null;
+console.log(typeof (j));
+let i=5;
+i+=5;
+console.log (i);
---
+++ Lectures/JS/var.js
@@ -0,0 +1,8 @@
+var a=10;
+console.log(a);
+var a=20;
+console.log(a);
+const a=10;
+console.log(a);
+const a=20;
+console.log(a);
